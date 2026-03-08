558c5a556bf224b6026e0a4b6144ca2aae609921
carcoragu <87833281+carcoragu@users.noreply.github.com>
2026-03-08 00:25:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
-// sw.js
 const CACHE_NAME = "agrosoft-campo-v2";
-const urlsToCache = ["./","index.html","manifest.json","logoisabsoft.png"];
+const urlsToCache = ["./","index.html","manifest.json","logoisabsoft.png","licencia.js","compra.js"];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
