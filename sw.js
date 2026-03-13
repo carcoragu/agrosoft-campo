a0e7919969a18e8df80eb37c9499777f83616408
carcoragu <87833281+carcoragu@users.noreply.github.com>
2026-03-13 12:26:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = "agrosoft-campo-v3";
-const urlsToCache = ["./","index.html","app.js","manifest.json","logoisabsoft.png"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+const CACHE_NAME="agrosoft-cache-v1";
+const urlsToCache=["./","./index.html","./manifest.json","./style.css","./app.js","./icon-192.png","./icon-512.png","./splash.png","./logo_fainver.png","./logoisabsoft.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
