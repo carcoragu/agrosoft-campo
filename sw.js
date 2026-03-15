a9246d5f420188650b08e174ae2e362f454e47b0
carcoragu <87833281+carcoragu@users.noreply.github.com>
2026-03-15 17:22:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-const CACHE_NAME="agrosoft-cache-v1";
-const urlsToCache=["./","./index.html","./manifest.json","./style.css","./app.js","./icon-192.png","./icon-512.png","./splash.png","./logo_fainver.png","./logoisabsoft.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE="agrosoft-v3"
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+])
+})
+)
+})
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request)
+})
+)
+})
